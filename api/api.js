550c63d1535fcb88e4798ca7250e483629e37b07
Tao. H <hamzatdestiny@gmail.com>
2023-01-08 00:48:45 +0100

    feat: Create and handle requests to new endpoint
    WIP: employees

--- api/api.js
+++ api/api.js
@@ -1,3 +1,7 @@
 const api = require("express").Router();
 
+// Mount /employees router
+const employees = require("./employees.js");
+api.use("/employees", employees);
+
 module.exports = api;
---
+++ api/employees.js
@@ -0,0 +1,68 @@
+const employees = require("express").Router();
+const sqlite3 = require("sqlite3");
+const db = new sqlite3.Database(
+  process.env.TEST_DATABASE || "./database.sqlite"
+);
+
+function validate(data) {
+  const { name, position, wage, isCurrentEmployee } = data;
+  if (!name || !position || !wage) {
+    return false;
+  }
+  return data;
+}
+
+// Retrieve all current Employees
+employees.get("/", (req, res, next) => {
+  const query = "SELECT * FROM Employee WHERE is_current_employee=1";
+  const params = [];
+  db.all(query, params, function (err, employees) {
+    if (err) {
+      return next(err);
+    }
+    return res.status(200).json({ employees: employees });
+  });
+});
+
+// Add a new Employee to the database
+employees.post("/", (req, res, next) => {
+  // Validate received data
+  const newEmployee = validate(req.body.employee);
+  // Handle incomplete/invalid data
+  if (newEmployee === false) {
+    return res.status(400).send();
+  }
+
+  // Add valid Employee data to db
+  const { name, position, wage, isCurrentEmployee } = newEmployee;
+  let query;
+  if (isCurrentEmployee === undefined) {
+    query =
+      "INSERT INTO Employee(name, position, wage) VALUES($name, $position, $wage)";
+  } else {
+    query = `INSERT INTO Employee(name, position, wage, is_current_employee) VALUES($name, $position, $wage, $isCurrentEmployee)`;
+  }
+  const params = {
+    $name: name,
+    $position: position,
+    $wage: wage,
+    $isCurrentEmployee: isCurrentEmployee,
+  };
+
+  db.run(query, params, function (err) {
+    if (err) {
+      return next(err);
+    }
+
+    // Retrieve and send newly created Employee
+    const query = `SELECT * FROM Employee WHERE id=${this.lastID}`;
+    db.get(query, [], function (err, newEmployee) {
+      if (err) {
+        return next(err);
+      }
+      return res.status(201).json({ employee: newEmployee });
+    });
+  });
+});
+
+module.exports = employees;
