3f0a4d60d054c94b6982d58efcbe10ffb2e0946d
Tao. H <hamzatdestiny@gmail.com>
2023-01-26 22:49:17 +0100

    feat: Add /timesheets GET request handler

--- api/timesheets.js
+++ api/timesheets.js
@@ -4,4 +4,17 @@ const db = new sqlite3.Database(
   process.env.TEST_DATABASE || "./database.sqlite"
 );
 
+timesheets.get("/", (req, res, next) => {
+  const query = `SELECT * FROM Timesheet WHERE employee_id=$employeeId`;
+  const params = {
+    $employeeId: req.params.employeeId,
+  };
+  db.all(query, params, function (err, timesheets) {
+    if (err) {
+      return next(err);
+    }
+    return res.status(200).json({ timesheets: timesheets });
+  });
+});
+
 module.exports = timesheets;
