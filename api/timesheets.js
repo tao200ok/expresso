31803349bc4fdce4a9e592736805cd84a9affa3a
Tao. H <hamzatdestiny@gmail.com>
2023-01-27 12:44:19 +0100

    feat: Add /timesheets/:id PUT request handler

--- api/timesheets.js
+++ api/timesheets.js
@@ -53,4 +53,60 @@ timesheets.post("/", (req, res, next) => {
   });
 });
 
+timesheets.param("timesheetId", (req, res, next, timesheetId) => {
+  const query = `SELECT * FROM Timesheet WHERE id=${timesheetId}`;
+  db.get(query, [], function (err, timesheet) {
+    if (err) {
+      return next(err);
+    }
+
+    // Timesheet with provided timesheetId not in database
+    if (!timesheet) {
+      res.status(404).json({ message: "No timesheet with that id" });
+    }
+
+    // Attach timesheet to the request object and proceed
+    req.timesheet = timesheet;
+    next();
+  });
+});
+
+timesheets.put("/:timesheetId", (req, res, next) => {
+  // Validate recieved data
+  const updatedTimesheet = validate(req.body.timesheet, [
+    "hours",
+    "rate",
+    "date",
+  ]);
+
+  // Handle incomplete timesheet data
+  if (updatedTimesheet === false) {
+    return res.status(400).send();
+  }
+
+  // Update employee data in database
+  const query = `UPDATE Timesheet SET hours=$hours, rate=$rate, date=$date WHERE id=$id`;
+  const { hours, rate, date } = updatedTimesheet;
+  const params = {
+    $hours: hours,
+    $rate: rate,
+    $date: date,
+    $id: req.params.timesheetId,
+  };
+  db.run(query, params, function (err) {
+    if (err) {
+      return next(err);
+    }
+
+    // Retrieve and send newly updated employee
+    const query = `SELECT * FROM Timesheet WHERE id=${req.params.timesheetId}`;
+    db.get(query, [], function (err, updatedTimesheet) {
+      if (err) {
+        return next(err);
+      }
+      return res.status(200).json({ timesheet: updatedTimesheet });
+    });
+  });
+});
+
 module.exports = timesheets;
