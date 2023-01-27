94bc4a1a8ed2c9dfaad9ccfbd3470cdbe867edc8
Tao. H <hamzatdestiny@gmail.com>
2023-01-27 16:07:38 +0100

    feat: Add /menus POST request handler

--- api/menus.js
+++ api/menus.js
@@ -3,6 +3,7 @@ const sqlite3 = require("sqlite3");
 const db = new sqlite3.Database(
   process.env.TEST_DATABASE || "./database.sqlite"
 );
+const { validate } = require("./utils");
 
 menus.get("/", (req, res, next) => {
   const query = "SELECT * FROM Menu";
@@ -11,6 +12,33 @@ menus.get("/", (req, res, next) => {
       return next(err);
     }
     return res.status(200).json({ menus: menus });
+  });
+});
+
+menus.post("/", (req, res, next) => {
+  // Validate received data
+  const newMenu = validate(req.body.menu, ["title"]);
+
+  // Handle incomplete/invalid data
+  if (newMenu === false) {
+    return res.status(400).send();
+  }
+
+  // Add valid Menu data to db
+  const query = `INSERT INTO Menu(title) VALUES('${newMenu.title}')`;
+  db.run(query, [], function (err) {
+    if (err) {
+      return next(err);
+    }
+
+    // Retrieve and send newly created Employee
+    const query = `SELECT * FROM Menu WHERE id=${this.lastID}`;
+    db.get(query, [], function (err, newMenu) {
+      if (err) {
+        return next(err);
+      }
+      return res.status(201).json({ menu: newMenu });
+    });
   });
 });
 
