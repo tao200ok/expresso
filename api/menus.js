786b98079fecc6646676c91ddaf6ed6b15abd245
Tao. H <hamzatdestiny@gmail.com>
2023-01-27 15:32:21 +0100

    feat: Add /menus GET request handler

--- api/menus.js
+++ api/menus.js
@@ -4,6 +4,16 @@ const db = new sqlite3.Database(
   process.env.TEST_DATABASE || "./database.sqlite"
 );
 
+menus.get("/", (req, res, next) => {
+  const query = "SELECT * FROM Menu";
+  db.all(query, [], function (err, menus) {
+    if (err) {
+      return next(err);
+    }
+    return res.status(200).json({ menus: menus });
+  });
+});
+
 // Mount /menu-items router
 const menuItems = require("./menu-items");
 menus.use("/:menuId/menu-items", menuItems);
