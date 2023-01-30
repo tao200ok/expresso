f38f60cfb7316d6e4ec7b1ed6b4d7985fb633e12
Tao. H <hamzatdestiny@gmail.com>
2023-01-30 16:48:14 +0100

    feat: Add .../menu-items GET request middleware

--- api/menu-items.js
+++ api/menu-items.js
@@ -4,4 +4,14 @@ const db = new sqlite3.Database(
   process.env.TEST_DATABASE || "./database.sqlite"
 );
 
+menuItems.get("/", (req, res, next) => {
+  let query = `SELECT * FROM MenuItem WHERE menu_id=${req.params.menuId}`;
+  db.all(query, [], function (err, menuItems) {
+    if (err) {
+      return next(err);
+    }
+    return res.status(200).json({ menuItems: menuItems });
+  });
+});
+
 module.exports = menuItems;
